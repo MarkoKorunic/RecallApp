7ebc4026a03282cbc53c24998cb3b456dcfa5775
Marko <marko.korunic29@gmail.com>
2020-08-27 17:30:30 +0200

    commit 27.8.2020.

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+
+    class Deck
+    {
+        string Name { get; set; }
+        DateTime DateCreated
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+        string CardList { get; set; }
+
+
+    }
+
+    class Card
+    {
+        Deck Deck { get; set; }
+        string Question { get; set; }
+        string Answer { get; set; }
+        DateTime DateCreated
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+        
+        
+
+
+    }
+
+}
+
+
